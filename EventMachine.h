3e913d4913c380bf9edea925d9590075cd77b103
Matheus Carmo <mematheuslc@gmail.com>
2018-04-24 22:38:43 -0300

    Declare EventMachine base class

---
+++ EventMachine.h
@@ -0,0 +1,16 @@
+// Copyright Carmel
+
+#include "linked_queue.h"
+
+class EventMachine {
+    private:
+        int occurred = 0;
+
+    public:
+        explicit EventMachine(int whenOccurred);
+        virtual ~EventMachine();
+        virtual void log();
+
+        virtual LinkedQueue<EventMachine*> runEvent();
+}
+
